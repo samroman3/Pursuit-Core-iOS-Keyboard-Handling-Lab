746744577c38e3017e3a1cf3940022c738c6a3f9
Sam <samroman3@gmail.com>
2019-10-17 14:59:15 -0400

    Adds methods for notification center

--- keyBoardHandlingLab/keyBoardHandlingLab/ViewController.swift
+++ keyBoardHandlingLab/keyBoardHandlingLab/ViewController.swift
@@ -11,14 +11,48 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    
+    
     @IBOutlet weak var scrollView: UIScrollView!
     
     
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    super.viewDidLoad()
+    configureScrollView()
+    NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardAppearing(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboardDisappearing(sender:)), name: UIResponder.keyboardWillShowNotification, object: nil)
     }
+    
+    
+    
+    //MARK: NotificationCenter Methods
+   @objc func handleKeyboardAppearing(sender: Notification) {
+        guard let infoDict = sender.userInfo else { return }
+        guard let rectValue = infoDict["UIKeyboardFrameEndUserInfoKey"] as? CGRect else { return }
+        print("The keyboard is \(rectValue.height) by \(rectValue.width)")
+    scrollView.frame.origin.y = -150
+    }
+    
+    @objc func handleKeyboardDisappearing(sender: Notification) {
+        guard let infoDict = sender.userInfo else { return }
+        guard let rectValue = infoDict["UIKeyboardFrameEndUserInfoKey"] as? CGRect else { return }
+        print("The keyboard is \(rectValue.height) by \(rectValue.width)")
+    scrollView.frame.origin.y = 150
+    }
+    
+    
+    
+    
+    
+    private func configureScrollView() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    }
+ 
 
 
 }
