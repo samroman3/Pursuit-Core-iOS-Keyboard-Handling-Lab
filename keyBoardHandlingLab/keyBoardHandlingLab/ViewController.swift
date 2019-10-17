cf2b9271b6620cd2838359d2702489ee8292b3b2
Sam <samroman3@gmail.com>
2019-10-17 14:40:44 -0400

    adds labels and storyboard elememnts

--- keyBoardHandlingLab/keyBoardHandlingLab/ViewController.swift
+++ keyBoardHandlingLab/keyBoardHandlingLab/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
